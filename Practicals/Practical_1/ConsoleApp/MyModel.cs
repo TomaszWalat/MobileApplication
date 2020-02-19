aec0d9e890f9a40a69ed97f48f64fadb1d0a1f42
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2020-02-19 10:13:23 +0000

    Working through chapter 1

---
+++ Practicals/Practical_1/ConsoleApp/MyModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class MyModel
+    {
+        public string FirstName { get; set; }
+        public string KnownAs { get; set; }
+        public int Age { get; set; }
+        public uint? PhoneNumber { get; set; }
+
+        public MyModel(string FirstName, int Age, string KnownAs=null, uint? PhoneNumber=null)
+        {
+            this.FirstName = FirstName;
+            this.KnownAs = KnownAs;
+            this.Age = Age;
+            this.PhoneNumber = PhoneNumber;
+        }
+
+        public void Display()
+        {
+            string outputString = $"This is {FirstName}";
+
+            if (KnownAs != null)
+            {
+                outputString += $", also known as {KnownAs}";
+            }
+
+            outputString += $", Age {Age}";
+
+            if(PhoneNumber != null)
+            {
+                outputString += $", Phone number {PhoneNumber}";
+            }
+
+            Console.WriteLine(outputString);
+
+            //Console.WriteLine($"This is {FirstName}, also known as {KnownAs}, Age {Age}, Phone Number {PhoneNumber}");
+        }
+
+    }
+}
---
+++ Practicals/Practical_1/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyModel m = new MyModel("Tomasz", 23, null, null);
+            m.Display();
+        }
+    }
+}
