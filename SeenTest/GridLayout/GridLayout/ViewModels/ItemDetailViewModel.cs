5add49d1f792910a6723da8c8818a1529361e782
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2020-05-05 19:24:01 +0100

    Wokring on section 1 of seen test

---
+++ SeenTest/GridLayout/GridLayout/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GridLayout.Models;
+
+namespace GridLayout.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ SeenTest/GridLayoutTutorial_b/GridLayoutTutorial/Margins.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GridLayoutTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Margins : ContentPage
+    {
+        public Margins()
+        {
+            InitializeComponent();
+        }
+    }
+}
